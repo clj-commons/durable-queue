e41aba55edf2b94ce8a3ab5b2f293bb05c4a5866
Bob Beaty <drbob@themanfromspud.com>
2017-06-08 15:30:17 -0500

    Changed project to Clojars
    I really wanted to get this up on Clojars, and this makes it very easy.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject factual/durable-queue "0.1.6"
+(defproject org.clojars.drbobbeaty/durable-queue "0.1.6"
   :description "a in-process task-queue that is backed by disk."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
