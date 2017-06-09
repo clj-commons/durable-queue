591e13c3aff044b82e61ab7a1392909bc88237c4
Bob Beaty <drbob@themanfromspud.com>
2017-06-09 16:23:53 -0500

    Fixed up the project for GPG
    I needed to clean up a few things to really make a decent try at GPG for
    the Clojar deployment. This is getting closer.

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
-(defproject org.clojars.drbobbeaty/durable-queue "0.1.6"
+(defproject org.clojars.drbobbeaty/durable-queue "0.1.7"
   :description "a in-process task-queue that is backed by disk."
+  :url "http://github.com/drbobbeaty/durable-queue"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.taoensso/nippy "2.8.0"]
                  [primitive-math "0.1.4"]
-                 [byte-streams "0.2.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                 [byte-streams "0.2.2"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [criterium "0.4.3"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
   :global-vars {*warn-on-reflection* true}
