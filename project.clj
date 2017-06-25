1225be493edbdc94af3a6abbe4d68e167f46634e
Bob Beaty <drbob@themanfromspud.com>
2017-06-25 18:49:54 -0500

    Few updates to make a cleaner diff
    I had a typo, and another addition to remove. SHould be clean now.

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject fractual/durable-queue "0.1.6"
+(defproject factual/durable-queue "0.1.6-SNAPSHOT"
   :description "a in-process task-queue that is backed by disk."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.taoensso/nippy "2.8.0"]
                  [primitive-math "0.1.4"]
                  [byte-streams "0.2.2"]]
-  :repositories [["releases" {:url "https://clojars.org/repo"
-                              :creds :gpg}]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [criterium "0.4.3"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
